bcaa0cbe4b58189bc328df22b0a8e59a1d17a0bd
Eduardo Carrrasco <carrascoe94@gmail.com>
2024-04-02 17:08:03 -0300

    First commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿for (int number = 1; number < 101; number++)
+{
+    if ((number % 3 == 0) && (number % 5 == 0)) Console.WriteLine($"{number} - FizzBuzz");
+    else if (number % 3 == 0) Console.WriteLine($"{number} - Fizz");
+    else if (number % 5 == 0) Console.WriteLine($"{number} - Buzz");
+    else Console.WriteLine($"{number}");
+}
